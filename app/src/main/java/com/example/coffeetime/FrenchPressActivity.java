486f8108f66c1c5d2c58601b95a74d9a590fc408
zachary mackeil <zack.mackeil@gmail.com>
2019-06-02 16:22:39 -0600

    Changed one variable name to allow for more patterns.
    Signed-off-by: zachary mackeil <zack.mackeil@gmail.com>

--- app/src/main/java/com/example/coffeetime/FrenchPressActivity.java
+++ app/src/main/java/com/example/coffeetime/FrenchPressActivity.java
@@ -1,12 +1,88 @@
 package com.example.coffeetime;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class FrenchPressActivity extends AppCompatActivity {
+    protected TextView cool;
+    protected TextView steep;
+    protected TextView Press;
+    protected Vibrator vibe;
+    private long[] pattern1 = {500, 150, 400, 150, 300, 50, 100};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.french_press);
+        cool = (TextView) findViewById(R.id.FPCoolTimer);
+        steep = (TextView) findViewById(R.id.FPSteepTimer);
+        Press = (TextView) findViewById(R.id.FPressTimer);
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    protected void startCoolTimer(View view){
+        new CountDownTimer(25000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                cool.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    vibe.vibrate(VibrationEffect.createWaveform(pattern1, 0));
+                }
+                else{
+                    vibe.vibrate(pattern1, 0);
+                }
+            }
+        }.start();
+    }
+
+    protected void startSteepTimer(View view){
+        new CountDownTimer(360000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    protected void startPressTimer(View view){
+        new CountDownTimer(30000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    //start the reset events
+    protected void resetCoolTimer(View view){
+
+    }
+
+    protected void resetSteepTimer(View view){
+
+    }
+
+    protected void resetPressTimer(View view){
+
     }
 }
