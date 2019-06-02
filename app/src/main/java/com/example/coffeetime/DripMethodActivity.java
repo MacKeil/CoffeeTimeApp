11b65b1ab0ac57db90c8c56013ffd564abf25aa8
Zachary MacKeil <zack.mackeil@gmail.com>
2019-06-02 13:47:10 -0600

    Implemented DripMethod countdown. Added vibration on countdown completion.
    Signed-off-by: Zachary <zack.mackeil@gmail.com>

--- app/src/main/java/com/example/coffeetime/DripMethodActivity.java
+++ app/src/main/java/com/example/coffeetime/DripMethodActivity.java
@@ -1,13 +1,49 @@
 package com.example.coffeetime;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class DripMethodActivity extends AppCompatActivity {
+    protected TextView DripTimer;
+    protected Vibrator vibe;
+    protected long[] pattern = {500,140,500,100,350};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drip_method);
+        DripTimer = (TextView) findViewById(R.id.dripTimer);
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    protected void onResetPress(View view){
+        vibe.cancel();
+        DripTimer.setText(Integer.toString(25));
+    }
+
+    public void onStartPress(View view){
+        new CountDownTimer(25000, 1000){
+            @Override
+            public void onTick(long l) {
+                DripTimer.setText(String.valueOf(l / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    vibe.vibrate(VibrationEffect.createWaveform(pattern, 0));
+                }
+                else{
+                    vibe.vibrate(pattern, 0);
+                }
+            }
+        }.start();
     }
 }
